06092003f82eb92c3de28f228e7041d5fd19a711
Yury Kiryla <yurykiryla@gmail.com>
2018-04-01 14:28:40 +0300

    complete collectors

--- java8streamAPI/src/com/java8streamapi/StreamsForLearn.java
+++ java8streamAPI/src/com/java8streamapi/StreamsForLearn.java
@@ -608,6 +608,155 @@ public class StreamsForLearn {
 		list2.forEach(System.out::println);
 		System.out.println();
 
+//        joining​()
+//        joining​(CharSequence delimiter)
+//        joining​(CharSequence delimiter, CharSequence prefix, CharSequence suffix)
+//        Собирает элементы, реализующие интерфейс CharSequence, в единую строку. Дополнительно можно указать
+//        разделитель, а также префикс и суффикс для всей последовательности.
+        String s1 = Stream.of("a", "b", "c", "d")
+                .collect(Collectors.joining());
+        System.out.println(s1);
+        String s2 = Stream.of("a", "b", "c", "d")
+                .collect(Collectors.joining("-"));
+        System.out.println(s2);
+        String s3 = Stream.of("a", "b", "c", "d")
+                .collect(Collectors.joining(" -> ", "[ ", " ]"));
+        System.out.println(s3);
+        System.out.println();
 
+//        summingInt​(ToIntFunction mapper)
+//        summingLong​(ToLongFunction mapper)
+//        summingDouble​(ToDoubleFunction mapper)
+//        Коллектор, который преобразовывает объекты в int/long/double и подсчитывает сумму.
+//
+//        averagingInt​(ToIntFunction mapper)
+//        averagingLong​(ToLongFunction mapper)
+//        averagingDouble​(ToDoubleFunction mapper)
+//        Аналогично, но со средним значением.
+//
+//        summarizingInt​(ToIntFunction mapper)
+//        summarizingLong​(ToLongFunction mapper)
+//        summarizingDouble​(ToDoubleFunction mapper)
+//        Аналогично, но с полной статистикой.
+        Integer sum = Stream.of("1", "2", "3", "4")
+                .collect(Collectors.summingInt(Integer::parseInt));
+        System.out.println(sum);
+        Double average = Stream.of("1", "2", "3", "4")
+                .collect(Collectors.averagingInt(Integer::parseInt));
+        System.out.println(average);
+        DoubleSummaryStatistics stats = Stream.of("1.1", "2.34", "3.14", "4.04")
+                .collect(Collectors.summarizingDouble(Double::parseDouble));
+        System.out.println(stats);
+        System.out.println();
+
+//        counting​()
+//        Подсчитывает количество элементов.
+        Long count = Stream.of("1", "2", "3", "4")
+                .collect(Collectors.counting());
+        System.out.println(count);
+        System.out.println();
+
+//        filtering​(Predicate predicate, Collector downstream)
+//        mapping​(Function mapper, Collector downstream)
+//        flatMapping​(Function downstream)
+//        reducing​(BinaryOperator op)
+//        reducing​(T identity, BinaryOperator op)
+//        reducing​(U identity, Function mapper, BinaryOperator op)
+//        Специальная группа коллекторов, которая применяет операции filter, map, flatMap и reduce. filtering​ и
+//        flatMapping​ появились в Java 9.
+        List<Integer> ints = Stream.of(1, 2, 3, 4, 5, 6)
+                .collect(Collectors.filtering(
+                        x -> x % 2 == 0,
+                        Collectors.toList()));
+        System.out.println(ints);
+        s1 = Stream.of(1, 2, 3, 4, 5, 6)
+                .collect(Collectors.filtering(
+                        x -> x % 2 == 0,
+                        Collectors.mapping(
+                                x -> Integer.toString(x),
+                                Collectors.joining("-")
+                        )
+                ));
+        System.out.println(s1);
+        s2 = Stream.of(2, 0, 1, 3, 2)
+                .collect(Collectors.flatMapping(
+                        x -> IntStream.range(0, x).mapToObj(Integer::toString),
+                        Collectors.joining(", ")
+                ));
+        System.out.println(s2);
+        int value = Stream.of(1, 2, 3, 4, 5, 6)
+                .collect(Collectors.reducing(
+                        0, (a, b) -> a + b
+                ));
+        System.out.println(value);
+        s3 = Stream.of(1, 2, 3, 4, 5, 6)
+                .collect(Collectors.reducing(
+                        "", x -> Integer.toString(x), (a, b) -> a + b
+                ));
+        System.out.println(s3);
+        System.out.println();
+
+//        minBy​(Comparator comparator)
+//        maxBy​(Comparator comparator)
+//        Поиск минимального/максимального элемента, основываясь на заданном компараторе.
+        Optional<String> min = Stream.of("ab", "c", "defgh", "ijk", "l")
+                .collect(Collectors.minBy(Comparator.comparing(String::length)));
+        min.ifPresent(System.out::println);
+        Optional<String> max = Stream.of("ab", "c", "defgh", "ijk", "l")
+                .collect(Collectors.maxBy(Comparator.comparing(String::length)));
+        max.ifPresent(System.out::println);
+
+//        groupingBy​(Function classifier)
+//        groupingBy​(Function classifier, Collector downstream)
+//        groupingBy​(Function classifier, Supplier mapFactory, Collector downstream)
+//        Группирует элементы по критерию, сохраняя результат в Map.
+//
+//        groupingByConcurrent​(Function classifier)
+//        groupingByConcurrent​(Function classifier, Collector downstream)
+//        groupingByConcurrent​(Function classifier, Supplier mapFactory, Collector downstream)
+//        Аналогичный набор методов, только сохраняет в ConcurrentMap.
+        Map<Integer, List<String>> map = Stream.of(
+                "ab", "c", "def", "gh", "ijk", "l", "mnop")
+                .collect(Collectors.groupingBy(String::length));
+        map.entrySet().forEach(System.out::println);
+        Map<Integer, String> map6 = Stream.of(
+                "ab", "c", "def", "gh", "ijk", "l", "mnop")
+                .collect(Collectors.groupingBy(
+                        String::length,
+                        Collectors.mapping(
+                                String::toUpperCase,
+                                Collectors.joining())
+                ));
+        map6.entrySet().forEach(System.out::println);
+        Map<Integer, List<String>> map7 = Stream.of(
+                "ab", "c", "def", "gh", "ijk", "l", "mnop")
+                .collect(Collectors.groupingBy(
+                        String::length,
+                        LinkedHashMap::new,
+                        Collectors.mapping(
+                                String::toUpperCase,
+                                Collectors.toList())
+                ));
+        map7.entrySet().forEach(System.out::println);
+        System.out.println();
+
+//        partitioningBy​(Predicate predicate)
+//        partitioningBy​(Predicate predicate, Collector downstream)
+//        Ещё один интересный метод. Разбивает последовательность элементов по какому-либо критерию. В одну часть
+//        попадают все элементы, которые удовлетворяют переданному условию, во вторую — все, которые не удовлетворяют.
+        Map<Boolean, List<String>> map8 = Stream.of(
+                "ab", "c", "def", "gh", "ijk", "l", "mnop")
+                .collect(Collectors.partitioningBy(s -> s.length() <= 2));
+        map8.entrySet().forEach(System.out::println);
+        Map<Boolean, String> map9 = Stream.of(
+                "ab", "c", "def", "gh", "ijk", "l", "mnop")
+                .collect(Collectors.partitioningBy(
+                        s -> s.length() <= 2,
+                        Collectors.mapping(
+                                String::toUpperCase,
+                                Collectors.joining())
+                ));
+        map9.entrySet().forEach(System.out::println);
+        System.out.println();
 	}
 }
