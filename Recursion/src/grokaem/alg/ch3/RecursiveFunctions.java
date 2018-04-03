c7b0425e02d7a0ecdcc01e8e8908ae769fb48126
Yury Kiryla <yury_kiryla@epam.com>
2018-04-03 12:03:24 +0300

    Recursive function

---
+++ Recursion/src/grokaem/alg/ch3/RecursiveFunctions.java
@@ -0,0 +1,18 @@
+package grokaem.alg.ch3;
+
+import java.security.InvalidParameterException;
+
+public class RecursiveFunctions {
+    public int factorial(int x) {
+        if (x < 1) {
+            throw new InvalidParameterException();
+        }
+
+        if (x == 1) {
+            return 1;
+        } else {
+            return x * factorial(x - 1);
+        }
+
+    }
+}
