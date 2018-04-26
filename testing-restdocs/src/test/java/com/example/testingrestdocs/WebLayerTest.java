ef689a0b9dc5f3f5d3b5840e3b7d177ae1ec4e46
Yury Kiryla <yury_kiryla@epam.com>
2018-04-26 11:50:00 +0300

    add rest doc

---
+++ testing-restdocs/src/test/java/com/example/testingrestdocs/WebLayerTest.java
@@ -0,0 +1,35 @@
+package com.example.testingrestdocs;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HomeController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
+public class WebLayerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("Hello world")))
+			.andDo(document("home"));
+	}
+
+}
