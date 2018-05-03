4b69b021cf4ddcf31230d6681d1732fc5133eb8c
Yury Kiryla <yury_kiryla@epam.com>
2018-05-03 09:15:33 +0300

    add task description

--- LeetCode/src/main/java/problems/p16/Solution.java
+++ LeetCode/src/main/java/problems/p16/Solution.java
@@ -3,6 +3,16 @@ package problems.p16;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+/**
+ * https://leetcode.com/problems/3sum-closest/description/
+ * 
+ * Given an array nums of n integers and an integer target, find three integers in nums such that 
+ * the sum is closest to target. Return the sum of the three integers. You may assume that each 
+ * input would have exactly one solution.
+ * 
+ * @author Yury_Kiryla
+ *
+ */
 public class Solution {
 	public int threeSumClosest(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
