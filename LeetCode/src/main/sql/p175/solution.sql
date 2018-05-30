b454219a447b67c68cad18cf9f070f46b6b25766
Yury Kiryla <yurykiryla@gmail.com>
2018-05-30 21:44:18 +0300

    175. Combine Two Tables

---
+++ LeetCode/src/main/sql/p175/solution.sql
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/combine-two-tables/description/
+
+
+select FirstName, LastName, City, State
+    from Person
+    left join Address
+    on Person.PersonId = Address.PersonId;
