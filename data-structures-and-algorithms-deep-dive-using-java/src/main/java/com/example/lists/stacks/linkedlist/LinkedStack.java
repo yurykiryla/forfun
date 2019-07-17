35952de95f891e2c6171a004b0ae9662e29f0e24
Yury Kiryla <yury_kiryla@epam.com>
2019-07-17 14:02:29 -0400

    add linked stack

---
+++ data-structures-and-algorithms-deep-dive-using-java/src/main/java/com/example/lists/stacks/linkedlist/LinkedStack.java
@@ -0,0 +1,42 @@
+package com.example.lists.stacks.linkedlist;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+import com.example.utils.Employee;
+
+public class LinkedStack {
+	
+	private LinkedList<Employee> stack;
+	
+	public LinkedStack() {
+		stack = new LinkedList<>();
+	}
+
+	public void push(Employee employee) {
+		stack.push(employee);
+	}
+	
+	public Employee pop() {
+		return stack.pop();
+	}
+	
+	public Employee peek() {
+		return stack.peek();
+	}
+	
+	public boolean isEmpty() {
+		return stack.isEmpty();
+	}
+	
+	public int size() {
+		return stack.size();
+	}
+	
+	public void printStack() {
+		ListIterator<Employee> iterator = stack.listIterator();
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
+}
---
+++ data-structures-and-algorithms-deep-dive-using-java/src/test/java/com/example/lists/stacks/linkedlist/LinkedStackTest.java
@@ -0,0 +1,88 @@
+package com.example.lists.stacks.linkedlist;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.EmptyStackException;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.lists.stacks.array.ArrayStack;
+import com.example.utils.Employee;
+
+class LinkedStackTest {
+
+	LinkedStack stack;
+
+	@Test
+	public void testPush() {
+		stack = new LinkedStack();
+
+		stack.push(new Employee("Frst", "Empl", 1));
+		stack.push(new Employee("Sec", "Empl", 2));
+		stack.push(new Employee("Trd", "Empl", 3));
+		stack.push(new Employee("For", "Empl", 4));
+
+		assertEquals(4, stack.size());
+	}
+
+	@Test
+	public void testPop() {
+		stack = new LinkedStack();
+
+		assertThrows(NoSuchElementException.class, () -> stack.pop());
+
+		Employee janeJones = new Employee("Jane", "Jones", 123);
+		Employee johnDoe = new Employee("John", "Doe", 4567);
+		Employee marySmith = new Employee("Mary", "Smith", 22);
+		Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+		stack.push(janeJones);
+		stack.push(johnDoe);
+		stack.push(marySmith);
+		stack.push(mikeWilson);
+
+		assertSame(mikeWilson, stack.pop());
+		assertSame(marySmith, stack.pop());
+
+		assertEquals(2, stack.size());
+	}
+
+	@Test
+	public void testPeak() {
+		stack = new LinkedStack();
+
+		assertNull(stack.peek());
+
+		Employee janeJones = new Employee("Jane", "Jones", 123);
+		Employee johnDoe = new Employee("John", "Doe", 4567);
+		Employee marySmith = new Employee("Mary", "Smith", 22);
+		Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+		stack.push(janeJones);
+		stack.push(johnDoe);
+		stack.push(marySmith);
+		stack.push(mikeWilson);
+
+		assertSame(mikeWilson, stack.peek());
+		assertSame(mikeWilson, stack.peek());
+
+		assertEquals(4, stack.size());
+	}
+
+	@Test
+	public void testIsEmpty() {
+		stack = new LinkedStack();
+
+		assertTrue(stack.isEmpty());
+
+		stack.push(new Employee("Frst", "Empl", 1));
+		stack.push(new Employee("Sec", "Empl", 2));
+		stack.push(new Employee("Trd", "Empl", 3));
+		stack.push(new Employee("For", "Empl", 4));
+
+		assertFalse(stack.isEmpty());
+	}
+
+}
