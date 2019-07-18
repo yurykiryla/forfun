5b2365e3076d59d3e2ce760a1d7d599957f01654
Yury Kiryla <yury_kiryla@epam.com>
2019-07-18 13:53:45 -0400

    fix imports

--- data-structures-and-algorithms-deep-dive-using-java/src/test/java/com/example/lists/stacks/linkedlist/LinkedStackTest.java
+++ data-structures-and-algorithms-deep-dive-using-java/src/test/java/com/example/lists/stacks/linkedlist/LinkedStackTest.java
@@ -1,14 +1,16 @@
 package com.example.lists.stacks.linkedlist;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.EmptyStackException;
 import java.util.NoSuchElementException;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.example.lists.stacks.array.ArrayStack;
 import com.example.utils.Employee;
 
 class LinkedStackTest {
